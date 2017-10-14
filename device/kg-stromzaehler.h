ca16e15fb2b0188898950d77d6a8378eb2d1afe7
ToSa27 <tobias@die-hoffs.net>
2017-10-14 23:07:50 +0200

    single phase watt meter

--- device/kg-stromzaehler.h
+++ device/kg-stromzaehler.h
@@ -1,15 +1,21 @@
 //env:TOSAESP_BOARD=nodemcuv2
 //env:TOSAESP_LIB_DEPS="OneWire, DallasTemperature, PCF8574, NewPing, HLW8012"
 
-#define TOSAESP_INPUTS   1
-byte inputPin[TOSAESP_INPUTS] = { 5 };
-byte inputTrigger[TOSAESP_INPUTS] = { RISING };
-byte inputType[TOSAESP_INPUTS] = { INPUT_PULLUP };
-byte inputDebounce[TOSAESP_INPUTS] = { 100 };
+#define TOSAESP_INPUTS   4
+byte inputPin[TOSAESP_INPUTS] = { 5, 4, 0, 2 };
+byte inputTrigger[TOSAESP_INPUTS] = { RISING, RISING, RISING, RISING };
+byte inputType[TOSAESP_INPUTS] = { INPUT_PULLUP, INPUT_PULLUP, INPUT_PULLUP, INPUT_PULLUP };
+byte inputDebounce[TOSAESP_INPUTS] = { 100, 100, 100, 100 };
 const char* inputName[TOSAESP_INPUTS] = {
-  "Drehstromzähler"
+  "Drehstromzähler",
+  "Waschmaschine",
+  "Serverschrank",
+  "Heizung"
 };
 const char* inputUoM[TOSAESP_INPUTS] = {
-  "Wh"
+  "Wh",
+  "1/2 Wh",
+  "1/2 Wh",
+  "1/2 Wh"
 };
   
